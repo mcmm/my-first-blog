e73c31d9a4d517d10e8d80952188652c34d95264
MC <mariacarlamm@hotmail.com>
2018-07-26 01:55:56 -0300

    get together

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^$', views.home, name='home'),
+    url(r'^tabuleiro$', views.tabuleiro, name='tabuleiro'),
+    url(r'^vd$', views.videogame, name='videogame')
 ]
