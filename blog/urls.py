8d90d5e84ed8873da4763c89eb455c74803e7b10
Maria Carla <mariacarlade@gmail.com>
2018-11-07 17:09:33 -0200

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^tabuleiro$', views.tabuleiro, name='tabuleiro'),
     url(r'^vd$', views.videogame, name='videogame'),
     url(r'^obrigada$', views.obrigada, name='obrigada'),
+    url(r'^google3d8ceef29a3d0cf1', name='google'),
 ]
